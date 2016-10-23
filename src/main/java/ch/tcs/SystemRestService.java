e048112041f67ac7a29cb79b09e5236bc6f928d7
thomas <oli307@yahoo.fr>
2016-10-23 20:49:03 +0200

    add more files

--- src/main/java/ch/tcs/SystemRestService.java
+++ src/main/java/ch/tcs/SystemRestService.java
@@ -1,6 +1,7 @@
 package ch.tcs;
 
 import org.json.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,23 +11,30 @@ import javax.ws.rs.core.Response;
 @Path("/system")
 public class SystemRestService
 {
+    @Autowired
+    private UserDao userDao;
+
     @GET
     @Path("/")
     @Produces("application/json")
     public Response printMessage()
     {
-        JSONObject json = new JSONObject();
-        JSONArray array=new JSONArray();
-        array.put("1");
-        array.put("2");
-        try {
+        try
+        {
+            JSONObject json = new JSONObject();
+            JSONArray array=new JSONArray();
+            array.put("1");
+            array.put("2");
             json.put("friends", array);
-        } catch (JSONException e) {
+            User user = new User("toto", "tutu");
+            userDao.save(user);
+            String result = json.toString();
+            return Response.status(200).entity(result).build();
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
-        String result = json.toString();
-
-        return Response.status(200).entity(result).build();
+        return null;
 
     }
 }
---
+++ src/test/java/ch/tcs/SystemRestServiceTest.java
@@ -0,0 +1,18 @@
+package ch.tcs;
+
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by thomas on 22/10/2016.
+ */
+public class SystemRestServiceTest
+{
+    @org.junit.Test
+    public void printMessage() throws Exception
+    {
+        Assert.assertEquals("a","a");
+    }
+
+}
